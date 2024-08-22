61bb608f7729130bc23186ea18bf82aff22c816e
Lorentz <25649139+lorentzforces@users.noreply.github.com>
2024-08-22 00:27:08 -0500

    clean up output formatting slightly

--- cmd/check-changes/main.go
+++ cmd/check-changes/main.go
@@ -11,8 +11,6 @@ import (
 
 // NOTE: for now, this only checks staged changes
 // TODO: accept a ref and diff against that ref
-// TODO: added todo line 1
-// TODO: added todo line 2
 func main() {
 	if !git.ExecExists() {
 		platform.FailOut("\"git\" executable not found on system PATH")
@@ -22,19 +20,19 @@ func main() {
 	platform.FailOnErr(err)
 
 	hasErrors := len(checkData.Errors) > 0
+	hasWarnings := len(checkData.Warnings) > 0
 	if hasErrors {
 		fmt.Println("POTENTIAL MAJOR ISSUES:")
 		for _, errorFlag := range checkData.Errors {
 			fmt.Printf("  - %s\n", errorFlag.Message())
-			fmt.Println("")
 		}
+		if hasWarnings { fmt.Println("") }
 	}
 
-	if len(checkData.Warnings) > 0 {
+	if hasWarnings {
 		fmt.Println("POTENTIAL ISSUES:")
 		for _, warnFlag := range checkData.Warnings {
 			fmt.Printf("  - %s\n", warnFlag.Message())
-			fmt.Println("")
 		}
 	}
 
